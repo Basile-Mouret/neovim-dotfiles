dafb04f63f80f286c0abb646d9c89e67b119222c
Basile-Mouret <basile.mouert234@gmail.com>
2025-07-03 15:09:02 +0200

    added typst opening in qute

--- lua/plugins/typst.lua
+++ lua/plugins/typst.lua
@@ -4,5 +4,7 @@ return {
   'chomosuke/typst-preview.nvim',
   lazy = false, -- or ft = 'typst' if you want it to load only for typst files
   version = '1.*',
-  opts = {}, -- lazy.nvim will implicitly call `setup {}`
+  opts = {
+    open_cmd = "qutebrowser %s", 
+  }, -- lazy.nvim will implicitly call `setup {}`
 }
